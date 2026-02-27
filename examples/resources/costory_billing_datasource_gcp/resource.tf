e2d46d06296346784d27a27f8ab512207f4b5fc1
Tanguy Compagnon <tanguycs@gmail.com>
2026-02-27 17:25:52 +0100

    Add more comments to terraform code (#5)
    * add more comments AWS
    
    * add more comments GCP
    
    * ok

--- examples/resources/costory_billing_datasource_gcp/resource.tf
+++ examples/resources/costory_billing_datasource_gcp/resource.tf
@@ -9,18 +9,23 @@ locals {
     "roles/bigquery.metadataViewer",
     "roles/bigquery.dataViewer",
   ])
+  # Set the BigQuery project, dataset, and table IDs where your detailed billing data is exported.
+  bigquery_project_id = "my-project"
+  bigquery_dataset_id = "billing_export"
+  bigquery_table_id   = "gcp_billing_export_v1_0123"
 }
 
 resource "google_bigquery_dataset_iam_member" "costory_access" {
   for_each   = local.bigquery_roles
-  dataset_id = "billing_export"
+  project    = local.bigquery_project_id
+  dataset_id = local.bigquery_dataset_id
   role       = each.key
   member     = "serviceAccount:${data.costory_service_account.current.service_account}"
 }
 
 resource "costory_billing_datasource_gcp" "main" {
   name                = "GCP Billing Export"
-  bq_uri              = "my-project.billing_export.gcp_billing_export_v1_0123"
+  bq_uri              = "${local.bigquery_project_id}.${local.bigquery_dataset_id}.${local.bigquery_table_id}"
   is_detailed_billing = true
   depends_on          = [google_bigquery_dataset_iam_member.costory_access]
 }
